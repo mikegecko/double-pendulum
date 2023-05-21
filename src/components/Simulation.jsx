2b4a42c33af1a8f6ffba6ea43c319523aa627cee
Michael Guedko <mjgeko@gmail.com>
2023-05-20 20:14:00 -0400

    Large commit
        - add drei
        - setup basic scene

--- src/components/Simulation.jsx
+++ src/components/Simulation.jsx
@@ -1,10 +1,30 @@
 import { Box } from "@mui/material";
-
+import { Canvas, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+import { Sphere, OrbitControls } from "@react-three/drei";
 
 export default function Simulation() {
+    const sceneRef = useRef();
+    
+
+
     return(
         <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '85vw', backgroundColor: '#2e2e2e'}}>
-            Sim space
+            <Canvas ref={sceneRef}>
+                <SceneInit />
+                <ambientLight />
+                <Sphere args={[39,16,16]}/>
+                <OrbitControls />
+            </Canvas>
         </Box>
     )
 }
+
+function SceneInit() {
+    const { scene, gl, camera } = useThree();
+    scene.background = "black";
+    
+    //gl.setClearColor(0x000000, 0.0);
+    camera.position.z = 100;
+  }
